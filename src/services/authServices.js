0f641198e918ccbb94b10ab9bb01b3162a6e7719
Carl Son Chang <changc5@lsbu.ac.uk>
2022-05-06 05:04:18 +0100

    commit

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -26,31 +26,6 @@ export const getTokenDetails = () => {
     fetch(apiendpoints.getToken(getCookie("jwt")), configs)
       .then((res) => res.json())
       .then((res) => resolve(res));
-    // resolve({
-    //   jwt: "eyJhbGciOiJIUzI1NiJ9.eyJhY2NvdW50SWQiOiIwNzk0MDMyMS02ZGMyLTQwYzgtOTQ4Zi00NzdjNzQ2YmM2ZTUiLCJzdWIiOiJwcmFrYXNobW9oaXQxQGdtYWlsLmNvbSIsImV4cCI6MTY0ODk1NzkzNywiaWF0IjoxNjQ3NzQ4MzM3LCJhY2NvdW50VXNlcklkIjoiZjdjNzEzYmUtYTcxOS00ZTVjLTgxZGYtZDI0OGRjYzZmMjIxIiwicmVmcmVzaFRva2VuIjoiNDgwN2I1ODktYjEyMS00MmY5LWE3MjQtMThlNDc4OTE3ZmIyIn0.mCvc9HA3DxCtMPkSr61sw9T0vDRKw1-gUbEhj72f6CQ",
-    //   user: {
-    //     accountId: "07940321-6dc2-40c8-948f-477c746bc6e5",
-    //     createdAt: 1647542782782,
-    //     createdBy: "f7c713be-a719-4e5c-81df-d248dcc6f221",
-    //     lastModifiedAt: 1647542782782,
-    //     lastModifiedBy: "f7c713be-a719-4e5c-81df-d248dcc6f221",
-    //     version: 1,
-    //     accountUserId: "f7c713be-a719-4e5c-81df-d248dcc6f221",
-    //     email: "prakashmohit1@gmail.com",
-    //     firstname: "Mohit",
-    //     lastname: "Prakash",
-    //     status: "ENABLED",
-    //     profilePictureDownloadURL: null,
-    //     accountUserType: "ADMIN",
-    //   },
-    //   tokenInfo: {
-    //     accountId: "07940321-6dc2-40c8-948f-477c746bc6e5",
-    //     sub: "prakashmohit1@gmail.com",
-    //     exp: 1648957937,
-    //     iat: 1647748337,
-    //     accountUserId: "f7c713be-a719-4e5c-81df-d248dcc6f221",
-    //     refreshToken: "4807b589-b121-42f9-a724-18e478917fb2",
-    //   },
-    // });
+   
   });
 };
